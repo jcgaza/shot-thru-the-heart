021fa533bb48404fc24774da9a926e281e12f215
jcgaza <jcgaza@up.edu.ph>
2018-11-16 04:09:02 +0800

    Initial working chat.

--- chat.py
+++ chat.py
@@ -1,69 +1,71 @@
 import socket
 import player_pb2
 import tcp_packet_pb2
-import struct
+
+from threading import Thread
 
 HOST = "202.92.144.45"
 PORT = 80
+BUFFER = 1024
 ADDRESS = (HOST, PORT)
+
 tcp = tcp_packet_pb2.TcpPacket()
-
-def mainMenu(option):
-  if (option == 1):
-    print("[1] Create Lobby")
-    print("[2] Join Lobby")
-    print("[3] Exit")
-  elif (option == 2):
-    print("[1] Chat")
-    print("[2] Disconnect")
-
-def createLobbyPocket():
-  lobby = tcp.CreateLobbyPacket()
-  lobby.type = tcp.CREATE_LOBBY
-
-  print("Enter max players: ", end="")
-  lobby.max_players = int(input())
-
-  s.send(lobby.SerializeToString())
-  data = s.recv(1024)
-  lobby.ParseFromString(data)
-
-  print(lobby.lobby_id)
-
-def connectPacket():
-  connect = tcp.ConnectPacket()
-  connect.type = tcp.CONNECT
-
-  print("Enter lobby ID: ", end="")
-  connect.lobby_id = str(input())
-  
-  newPlayer = player_pb2.Player()
-  
-  print("Enter player name: ", end="")
-  connect.player.name = input()
-
-  s.send(connect.SerializeToString())
-  data = s.recv(1024)
-  connect.ParseFromString(data)
-  print(connect.player)
+connect = tcp.ConnectPacket()
+connect.type = tcp.CONNECT
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect(ADDRESS)
-  option = 1
+
+  # Create Lobby Packet
+  if input("Create lobby? [Y/N] ").lower() == "y":
+    lobby = tcp.CreateLobbyPacket()
+    lobby.type = tcp.CREATE_LOBBY
+
+    print("Enter max players: ", end="")
+    lobby.max_players = int(input())
+
+    s.send(lobby.SerializeToString())
+    data = s.recv(1024)
+    lobby.ParseFromString(data)
+    lobbyID = lobby.lobby_id
+  else:
+    lobbyID = input("Enter lobby ID: ")
+    # TODO: add error LDNE here
+
+  print("You're in Lobby {}.".format(lobbyID))
+  # Connect Packet
+  tcp.type = tcp.CONNECT
+  connect.player.name = input("Enter name: ")
+  connect.lobby_id = lobbyID
+
+  s.send(connect.SerializeToString())
+  
+  chat = tcp.ChatPacket()
+  chat.type = tcp.CHAT
+  chat.player.name = connect.player.name
+
+  def parser(data):
+    tcp.ParseFromString(data)
+
+    if (tcp.type == tcp.CONNECT):
+      connect.ParseFromString(data)
+      print("{} joined.".format(connect.player.name))
+    elif (tcp.type == tcp.CHAT):
+      chat.ParseFromString(data)
+      print("{}: {}".format(chat.player.name, chat.message))
+
+  def receivePacket(parser):
+    while True:
+      try:
+        data = s.recv(BUFFER)
+        parser(data)
+      except OSError:
+        break
+
+  receiver = Thread(target=receivePacket, args=[parser])
+  receiver.start()
 
   while True:
-    mainMenu(option)
-    
-    print("Choose: ", end="")
-    choice = int(input())
-    if (option == 1 and choice == 1):
-      createLobbyPocket()
-      option += 1
-    elif (option == 1 and choice == 2):
-      connectPacket()
-    elif (option == 1 and choice == 3):
-      break
-    elif (option == 2 and choice == 1):
-      connectPacket()
-    elif (option == 2 and choice == 2):
-      option -= 1
+    message = input("Message: ")
+    chat.message = message
+    s.send(chat.SerializeToString())
